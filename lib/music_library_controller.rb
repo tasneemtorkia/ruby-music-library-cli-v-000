f0c2a7018f85249aebef112a28dc983071f90cc7
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-08-15 18:27:44 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -83,11 +83,14 @@ def list_songs_by_genre
 end 
 end 
 
-def play_song 
-  puts "Which song number would you like to play?"
-  input = gets.chomp.strip.to_i
-    if (1..Song.all.length).include?(input)
-      puts "Playing #{song.name} by #{song.artist.name}" if song
-end 
-end 
-end 
+ def play_song
+    puts "Which song number would you like to play?"
+ 
+    input = gets.strip.to_i
+        if (1..Song.all.length).include?(input)
+      song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
+    end
+ 
+    puts "Playing #{song.name} by #{song.artist.name}" if song
+  end
+end
